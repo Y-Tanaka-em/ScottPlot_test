db169724f9cfe8215816d1bf18c36f7545474728
Tanaka Yojiro <yojiro42600@gmail.com>
2024-10-31 18:11:53 +0900

    プロジェクト ファイルを追加します。

---
+++ ScottPlot_test/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace ScottPlot_test
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.formsPlot1 = new ScottPlot.FormsPlot();
+            this.update = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // formsPlot1
+            // 
+            this.formsPlot1.Location = new System.Drawing.Point(12, 11);
+            this.formsPlot1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.formsPlot1.Name = "formsPlot1";
+            this.formsPlot1.Size = new System.Drawing.Size(400, 277);
+            this.formsPlot1.TabIndex = 0;
+            // 
+            // update
+            // 
+            this.update.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.update.Location = new System.Drawing.Point(12, 293);
+            this.update.Name = "update";
+            this.update.Size = new System.Drawing.Size(400, 39);
+            this.update.TabIndex = 1;
+            this.update.Text = "update";
+            this.update.UseVisualStyleBackColor = true;
+            this.update.Click += new System.EventHandler(this.update_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 362);
+            this.Controls.Add(this.update);
+            this.Controls.Add(this.formsPlot1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private ScottPlot.FormsPlot formsPlot1;
+        private System.Windows.Forms.Button update;
+    }
+}
+
---
+++ ScottPlot_test/Form1.cs
@@ -0,0 +1,55 @@
+﻿using ScottPlot.Plottable;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScottPlot_test
+{
+    public partial class Form1 : Form
+    {
+        /* 描画するデータの作成 */
+        Random rand = new Random(0);
+        double[] pointX = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+        double[] pointY = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            /* グラフのタイトル・縦/横軸ラベルの設定 */
+            formsPlot1.Plot.Title("Signal Plot サンプル");
+            formsPlot1.Plot.YAxis.Label(label: "Y軸データ");
+            formsPlot1.Plot.XAxis.Label(label: "X軸データ");
+
+            /* データのセット */
+            formsPlot1.Plot.AddScatter(pointX, pointY, label: "データ");
+
+            /* 凡例を表示 */
+            formsPlot1.Plot.Legend();
+
+            /* グラフ表示の上限下限を設定 */
+            formsPlot1.Plot.SetAxisLimits(xMin: 0, xMax: 6, yMin: 0, yMax: 6);
+            /* ScottPlotのコントロールに描画 */
+            formsPlot1.Render();
+
+        }
+
+        private void update_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < pointY.Length; i++)
+            {
+                pointY[i] = rand.Next(1, 5);
+            }
+
+            /* ScottPlotのコントロールに描画 */
+            formsPlot1.Render();
+        }
+    }
+}
